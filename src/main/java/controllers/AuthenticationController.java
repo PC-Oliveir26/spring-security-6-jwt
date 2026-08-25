775d9fcee6f9aa50ad0a10015e3dfeb95e55f41e
PCOliver26 <priscilacamileoliveira62@gmail.com>
2026-08-25 14:24:05 -0300

    authentication

--- src/main/java/controllers/AuthenticationController.java
+++ src/main/java/controllers/AuthenticationController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class AuthenticationController {
 	}
 	
 	@PostMapping("authenticate")
-	public String authenticate() {
-		return authenticationService.authentication();
+	public String authenticate(Authentication authentication) {
+		return authenticationService.authentication(authentication);
 	}
 }
--- src/main/java/servers/AuthenticationServicer.java
+++ src/main/java/servers/AuthenticationServicer.java
@@ -1,11 +1,18 @@
 package servers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationServicer {
+	
+	private final JwtService jwtService;
+	
+	public AuthenticationServicer ( JwtService jwtService ) {
+		this.jwtService = jwtService;
+	}
 
-	public String authentication() {
-		return "token";
+	public String authentication(Authentication authentication) {
+		return jwtService.generateToken(authentication);
 	}
 }
